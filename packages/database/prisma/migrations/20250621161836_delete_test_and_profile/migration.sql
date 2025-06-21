25b6ba6c07c4635fedad13c8336f443c684501ac
trupletj <tuvshinjargal01@gmail.com>
2025-06-22 00:18:47 +0800

    migration

---
+++ packages/database/prisma/migrations/20250621161836_delete_test_and_profile/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Profile` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `test` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "Profile";
+
+-- DropTable
+DROP TABLE "test";
