e89d2adf0a54e9df186a6e3c1ec1b482164a045d
aagiihhz <edom.altansuh@gmail.com>
2025-06-28 18:01:15 +0800

    rating develop

--- apps/admin/components/rating-dialog-content.tsx
+++ apps/admin/components/rating-dialog-content.tsx
@@ -79,10 +79,7 @@ const RatingDialogContent = ({
     clauseJob: JobPositionWithOrganization
   ) => {
     setSelectedClauseJobPosition(clauseJob);
-
   };
-
-
 
   return (
     <DialogContent className="sm:max-w-[600px] lg:max-w-[1000px] w-full overflow-y-auto h-[80vh]">
@@ -126,50 +123,55 @@ const RatingDialogContent = ({
         <ResizableHandle />
 
         {selectedClauseJobPosition ? (
-          <RatingForm selectedClauseJobPositionId={selectedClauseJobPosition?.id} position_name={selectedClauseJobPosition?.job_position.name} /> 
-        ) : <ResizablePanel defaultSize={40} minSize={35}><div className="flex flex-col gap-4 h-full p-6 overflow-auto min-h-[600px]"> Ажлын байр сонгоно уу </div></ResizablePanel>}
+          <RatingForm
+            selectedClauseJobPositionId={selectedClauseJobPosition?.id}
+            position_name={selectedClauseJobPosition?.job_position.name}
+          />
+        ) : (
+          <ResizablePanel defaultSize={40} minSize={35}>
+            <div className="flex flex-col gap-4 h-full p-6 overflow-auto min-h-[600px]">
+              {" "}
+              Ажлын байр сонгоно уу{" "}
+            </div>
+          </ResizablePanel>
+        )}
       </ResizablePanelGroup>
-
     </DialogContent>
   );
 };
 
 export default RatingDialogContent;
 
-
 function RatingForm({
   selectedClauseJobPositionId,
   position_name,
 }: {
-  selectedClauseJobPositionId: string,
-  position_name: string | null
-
+  selectedClauseJobPositionId: string;
+  position_name: string | null;
 }) {
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
   const [ratings, setRatings] = useState<Rating[]>([]);
-  const [rating, setRating] = useState<Partial<Rating>>({ score: 1, description: null })
-
+  const [rating, setRating] = useState<Partial<Rating>>({
+    score: 1,
+    description: null,
+  });
 
   const fetchRatings = async () => {
-    setIsLoading(true)
+    setIsLoading(true);
     const ratings = (await getRatings({
       orderBy: { scored_date: "desc" },
       where: { clause_job_position_id: selectedClauseJobPositionId },
     })) as Rating[];
-    setRatings(ratings)
-    setIsLoading(false)
+    setRatings(ratings);
+    setIsLoading(false);
   };
 
   useEffect(() => {
-    if (selectedClauseJobPositionId) fetchRatings()
+    if (selectedClauseJobPositionId) fetchRatings();
   }, [selectedClauseJobPositionId]);
 
-
-
-  const handleSubmit = async (
-    e: React.FormEvent<HTMLFormElement>
-  ) => {
-    e.preventDefault()
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     if (!selectedClauseJobPositionId) return;
 
     if (rating?.score === 6 && !rating?.description) {
@@ -190,11 +192,10 @@ function RatingForm({
         });
         if (result) {
           toast.success("Үнэлгээ амжилттай хадгалагдлаа!", { autoClose: 1500 });
-          await fetchRatings()
-        }else{
-          setIsLoading(false)
+          await fetchRatings();
+        } else {
+          setIsLoading(false);
         }
-
       } else {
         toast.warning("Үнэлгээ заавал өгнө үү!", { autoClose: 1500 });
       }
@@ -204,109 +205,108 @@ function RatingForm({
     }
   };
 
-  return <ResizablePanel defaultSize={40} minSize={35}>
-    <div className="flex flex-col gap-4 h-full p-6 overflow-auto">
-      <div className="text-center">
-        <h2 className="text-xl font-semibold">
-          {position_name && position_name}
-        </h2>
-        <div className="text-muted-foreground text-sm mt-2">
-          {/* {selectedClausePosition.job_position?.organization?.name} ---{" "}
+  return (
+    <ResizablePanel defaultSize={40} minSize={35}>
+      <div className="flex flex-col gap-4 h-full p-6 overflow-auto">
+        <div className="text-center">
+          <h2 className="text-xl font-semibold">
+            {position_name && position_name}
+          </h2>
+          <div className="text-muted-foreground text-sm mt-2">
+            {/* {selectedClausePosition.job_position?.organization?.name} ---{" "}
                   {
                     actionTypes.find(
                       (type) => type.value === selectedClausePosition.type
                     )?.label
                   } */}
-        </div>
-      </div>
-
-      <form
-        className="space-y-4"
-        onSubmit={(e) => handleSubmit(e)
-        }
-      >
-        <div className="flex items-center space-x-3 justify-between">
-          <div className="flex items-center space-x-3 my-2">
-            <label className="block font-medium">Оноо (1-6):</label>
-            <select
-              name="score"
-              className="border px-3 rounded-md w-max-[120px]"
-              onChange={(e) =>
-                setRating((prev) => ({
-                  ...prev,
-                  score: Number(e.target.value),
-                  description : null
-                }))
-              }
-            >
-              <option value={rating?.score}  >-- Оноо сонгоно уу --</option>
-              {[1, 2, 3, 4, 5, 6].map((n) => (
-                <option key={n} value={n}>
-                  {n}
-                </option>
-              ))}
-            </select>
-
           </div>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Button type="submit" className="w-max-[40px]">
-                <Star />
-              </Button>
-            </TooltipTrigger>
-
-            <TooltipContent className="h-max-[20px]">
-              Үнэлгээ өгөх
-            </TooltipContent>
-          </Tooltip>
         </div>
 
-     
+        <form className="space-y-4" onSubmit={(e) => handleSubmit(e)}>
+          <div className="flex items-center space-x-3 justify-between">
+            <div className="flex items-center space-x-3 my-2">
+              <label className="block font-medium">Оноо (1-6):</label>
+              <select
+                name="score"
+                className="border px-3 rounded-md w-max-[120px]"
+                onChange={(e) =>
+                  setRating((prev) => ({
+                    ...prev,
+                    score: Number(e.target.value),
+                    description: null,
+                  }))
+                }
+              >
+                <option value={rating?.score}>-- Оноо сонгоно уу --</option>
+                {[1, 2, 3, 4, 5, 6].map((n) => (
+                  <option key={n} value={n}>
+                    {n}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button type="submit" className="w-max-[40px]">
+                  <Star />
+                </Button>
+              </TooltipTrigger>
+
+              <TooltipContent className="h-max-[20px]">
+                Үнэлгээ өгөх
+              </TooltipContent>
+            </Tooltip>
+          </div>
+
           <div>
             <label className="block mb-1 font-medium">Тайлбар:</label>
             <textarea
               name="description"
               className="w-full border px-3 py-2 rounded-md"
               rows={3}
-              value={rating?.description ? rating?.description : "" }
+              value={rating?.description ? rating?.description : ""}
               placeholder="Тайлбараа бичнэ үү..."
               disabled={rating.score !== 6 || isLoading}
               onChange={(e) =>
                 setRating((prev) => ({
                   ...prev,
-                  description: e.target.value
+                  description: e.target.value,
                 }))
               }
             />
           </div>
-    
 
-        {isLoading ? <div> Ачааллаж байна</div> : <div>
-          <h4 className="font-semibold mb-2">Үнэлгээний жагсаалт:</h4>
-          {ratings.length === 0 ? (
-            <p className="text-muted-foreground">Үнэлгээ олдсонгүй.</p>
+          {isLoading ? (
+            <div> Ачааллаж байна</div>
           ) : (
-            <ul className="space-y-2">
-              {ratings.map((rating: Rating) => (
-                <li
-                  key={rating.id}
-                  className="border p-3 rounded-md bg-muted/30"
-                >
-                  <div>
-                    Огноо:{" "}
-                    {new Date(rating.scored_date).toLocaleDateString()}
-                  </div>
-                  <div>Оноо: {rating.score}</div>
+            <div>
+              <h4 className="font-semibold mb-2">Үнэлгээний жагсаалт:</h4>
+              {ratings.length === 0 ? (
+                <p className="text-muted-foreground">Үнэлгээ олдсонгүй.</p>
+              ) : (
+                <ul className="space-y-2">
+                  {ratings.map((rating: Rating) => (
+                    <li
+                      key={rating.id}
+                      className="border p-3 rounded-md bg-muted/30"
+                    >
+                      <div>
+                        Огноо:{" "}
+                        {new Date(rating.scored_date).toLocaleDateString()}
+                      </div>
+                      <div>Оноо: {rating.score}</div>
 
-                  {rating.description && (
-                    <div>Тайлбар: {rating.description}</div>
-                  )}
-                </li>
-              ))}
-            </ul>
+                      {rating.description && (
+                        <div>Тайлбар: {rating.description}</div>
+                      )}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </div>
           )}
-        </div>}
-      </form>
-    </div>
-  </ResizablePanel>
+        </form>
+      </div>
+    </ResizablePanel>
+  );
 }
